bb48090a83227a7ab275ecdd5c9ab0b7ea7e1ac3
rudedanko <mariamrudenko@gmail.com>
2023-03-17 10:27:42 -0400

    Variety of Assignments

---
+++ lectures/lecture16/answers/01-warmup/index.js
@@ -0,0 +1,19 @@
+function toggleFall() {
+    document.querySelector('h1').innerText = "Fall in Asheville";
+    document.querySelector('body').className = "fall";
+}
+
+function toggleWinter() {
+    document.querySelector('h1').innerText = "Winter in Asheville";
+    document.querySelector('body').className = "winter";
+}
+
+function toggleSpring() {
+    document.querySelector('h1').innerText = "Spring in Asheville";
+    document.querySelector('body').className = "spring";
+}
+
+function toggleUNCA() {
+    document.querySelector('h1').innerText = "UNCA"
+    document.querySelector('body').className = "unca";
+}
