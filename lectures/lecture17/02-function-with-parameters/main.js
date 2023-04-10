f5c39c7f81a0c185e2ddd5380ebff342a1e7f31c
rudedanko <mariamrudenko@gmail.com>
2023-04-10 01:58:06 -0400

    lecture 17

--- lectures/lecture17/02-function-with-parameters/main.js
+++ lectures/lecture17/02-function-with-parameters/main.js
@@ -1,19 +1,16 @@
-// make a function:
+// function makeRed(){
+//     document.querySelector('body').style.backgroundColor ='red';
+// };
 
-// function makeRed() {
-//     document.querySelector('body').style.backgroundColor = 'red';
-// }
+// function makeBlue(){
+//     document.querySelector('body').style.backgroundColor ='blue';
+// };
 
-// function makeBlue() {
-//     document.querySelector('body').style.backgroundColor = 'blue';
-// }
+// function makeGreen(){
+//     document.querySelector('body').style.backgroundColor ='green';
+// };
 
-
-// function makeGreen() {
-//     document.querySelector('body').style.backgroundColor = 'green';
-// }
-
-function changeColor(bgcolor, textcolor) {
-    document.querySelector('body').style.backgroundColor = bgcolor;
-    document.querySelector('body').style.color = textcolor;
+function changeColor(color, textColor) {
+    document.querySelector('body').style.backgroundColor =color;
+    document.querySelector('body').style.color =textColor;
 }
--- lectures/lecture17/03-drawings/sketch.js
+++ lectures/lecture17/03-drawings/sketch.js
@@ -2,37 +2,59 @@ function setup() {
     const canvasWidth = window.innerWidth;
     const canvasHeight = window.innerHeight; 
     createCanvas(canvasWidth, canvasHeight);
-    background("#EEE");
+    background("lightblue");
 
     /* Use the drawing functions to draw a picture. Ideas:
         * Draw an animal
         * Draw a tree
         * Draw a car
-        * Draw some abstract art 
+        * Draw some abstract art
     */
 
-    // https://p5js.org/reference/#/p5/circle
-    fill("black")
-    circle(700,200, 120);
-    circle(745,325, 50);
-    circle(650,325, 50);
-    rect(670, 250, 55, 300);
-    circle(650,550, 50);
-    circle(740,550, 50);
+    https://p5js.org/reference/#/p5/circle
+    fill("#5C4033")
+    circle(700, 400, 600);
+
+    https://p5js.org/reference/#/p5/circle
+    fill("white")
+    circle(600, 300, 100);
+
+    https://p5js.org/reference/#/p5/circle
+    fill("blue")
+    circle(600, 300, 50);
+
+    https://p5js.org/reference/#/p5/circle
+    fill("white")
+    circle(610, 290, 10);
+
+    https://p5js.org/reference/#/p5/circle
+    fill("white")
+    circle(800, 300, 100);
+
+    https://p5js.org/reference/#/p5/circle
+    fill("blue")
+    circle(800, 300, 50);
+
+    https://p5js.org/reference/#/p5/circle
+    fill("white")
+    circle(810, 290, 10);
 
     // https://p5js.org/reference/#/p5/rect
+ fill('white')
+ rect(650, 490, 100, 100);
+
+    https://p5js.org/reference/#/p5/ellipse
+    fill('lightpink');
+    ellipse(700, 450, 200, 100);
+
 
 
     // add your drawing here:
     // https://p5js.org/reference/#/p5/point
-    point(110, 20);
+    // point(110, 20);
 
     // https://p5js.org/reference/#/p5/text
     // text("Here is some text", 200, 40);
-
-    // https://p5js.org/reference/#/p5/ellipse
-    // fill('red');
-    // ellipse(100, 200, 60, 100);
 
     // Other shapes...
     // Polygon: https://p5js.org/reference/#/p5/beginShape
